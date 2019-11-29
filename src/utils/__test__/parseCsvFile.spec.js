f959df3164f1b5daa7749e41ee413e1bb4b6cce1
Rockie Guo <rockie@kitmi.com.au>
2019-11-29 14:50:24 +1100

    Fix csv parser.

--- src/utils/__test__/parseCsvFile.spec.js
+++ src/utils/__test__/parseCsvFile.spec.js
@@ -5,9 +5,21 @@ const parseCsvFile = require('../parseCsvFile');
 
 const csvFile = path.resolve(__dirname, '../../../test/files/australia.csv');
 
-describe('unit:parseCsvFile', function () {    
+describe.only('unit:parseCsvFile', function () {    
     it('parse csv', async function () {
         let result = await parseCsvFile(csvFile);
-        console.log(result);
+        result.length.should.be.exactly(70);
+        result[69].ticker.should.be.equal('BTCA');
+    });
+
+    it('parse csv with transform stream', async function () {
+        let result = [];
+
+        await parseCsvFile(csvFile, null, async (record, line) => {
+            result.push({ id: line, ...record });
+        });
+        result.length.should.be.exactly(70);
+        result[69].ticker.should.be.equal('BTCA');
+        result[69].id.should.be.exactly(69);
     });
 });
--- src/utils/parseCsvFile.js
+++ src/utils/parseCsvFile.js
@@ -2,9 +2,7 @@ const { fs, Promise } = require('rk-utils');
 const { tryRequire } = require('./lib');
 
 module.exports = async (csvFile, options, transformer) => {
-    const parse = tryRequire('csv-parse');
-
-    const output = []
+    const parse = tryRequire('csv-parse');    
 
     const readStream = fs.createReadStream(csvFile);
     const parser = parse({
@@ -12,31 +10,42 @@ module.exports = async (csvFile, options, transformer) => {
         ...options
     });
 
-    const read = transformer ? () => {
-        let record;
-        while (record = parser.read()) {
-            let transformed = transformer(record);
-            if (transformed) output.push(transformed);
-        }
-    } : () => {
-        let record;
-        while (record = parser.read()) {
-            output.push(record);
-        }
-    };
+    let transformSteam, output;
+
+    if (transformer) {
+        const transform = tryRequire('stream-transform');
+        let line = 0;
+
+        transformSteam = transform((data, callback) => {
+            transformer(data, line++).then(result => callback(null, result)).catch(error => callback(error));
+        });
+    } else {
+        output = [];
+    }   
 
     return new Promise((resolve, reject) => {
-        parser.on('readable', read);
-
         // Catch any error
-        parser.on('error', err => {
-            reject(err);
-        });
+        parser.on('error', reject); 
 
-        // When we are done, test that the parsed output matched what expected
-        parser.on('end', () => resolve(output));
+        if (!transformSteam) {
+            parser.on('readable', () => {   
+                let record;
+                             
+                while (record = parser.read()) {
+                    output.push(record);
+                }
+            });
+            
+            // When we are done, test that the parsed output matched what expected
+            parser.on('end', () => resolve(output));
 
-        readStream.pipe(parser);
+            readStream.pipe(parser);
+        } else {
+            transformSteam.on('error', reject);
+            transformSteam.on('finish', resolve);
+
+            readStream.pipe(parser).pipe(transformSteam);
+        }     
     });    
 };
     
