7df182e63451e4b3773ddebf547e5d1bb35ab27b
Rockie Guo <rockie@kitmi.com.au>
2020-03-03 18:32:54 +1100

    Change db error to exposable

--- src/utils/Errors.js
+++ src/utils/Errors.js
@@ -1,17 +1,17 @@
 "use strict";
 
-const { Errors: { InvalidArgument, ApplicationError, ExposableError } } = require('@genx/app');
+const { Errors: { InvalidArgument, ExposableError } } = require('@genx/app');
 const HttpCode = require('http-status-codes');
 
 class ValidationError extends ExposableError {
     constructor(message, info) {
         super(message, info, HttpCode.BAD_REQUEST, 'E_INVALID_DATA');
     }
-} 
+}
 
-class DatabaseError extends ApplicationError {
+class DatabaseError extends ExposableError {
     constructor(message, info) {
-        super(message, info, 'E_DATABASE');
+        super(message, info, HttpCode.INTERNAL_SERVER_ERROR, 'E_DATABASE');
     }
 }
 
