480f4c8e98254c722391d9ea2025d5cba5fb28d4
Rockie Guo <rockie@kitmi.com.au>
2020-12-12 14:58:58 +1100

    Separate jes into @genx/jes

--- src/utils/Errors.js
+++ src/utils/Errors.js
@@ -1,36 +1,5 @@
 "use strict";
 
-const { Errors: { InvalidArgument, ExposableError, ApplicationError } } = require('@genx/app');
-const HttpCode = require('http-status-codes');
+const Errors = require("@genx/error");
 
-//suggest use { data, ... } as info
-class ValidationError extends ExposableError {
-    constructor(message, info) {
-        super(message, info, HttpCode.BAD_REQUEST, 'E_INVALID_DATA');
-    }
-}
-
-class ReferencedNotExistError extends ExposableError {
-    constructor(message, info) {
-        super(message, info, HttpCode.BAD_REQUEST, 'E_REFERENCED_NOT_EXIST');
-    }
-}
-
-class DuplicateError extends ExposableError {
-    constructor(message, info) {
-        super(message, info, HttpCode.BAD_REQUEST, 'E_DUPLICATE');
-    }
-}
-
-class DatabaseError extends ApplicationError {
-    constructor(message, info) {
-        super(message, info, HttpCode.INTERNAL_SERVER_ERROR, 'E_DATABASE');
-    }
-}
-
-exports.InvalidArgument = InvalidArgument;
-exports.ValidationError = ValidationError;
-exports.ReferencedNotExistError = ReferencedNotExistError;
-exports.DuplicateError = DuplicateError;
-exports.DatabaseError = DatabaseError;
-exports.ApplicationError = ApplicationError;
+module.exports = Errors;
