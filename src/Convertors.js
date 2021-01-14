e43983eb5374bf9d341f29a9aa3f11aa53df524a
Rockie Guo <rockie@kitmi.com.au>
2021-01-14 22:00:44 +1100

    Refine remap.

--- src/Convertors.js
+++ src/Convertors.js
@@ -2,6 +2,7 @@
 
 const validator = require("validator");
 const { _ } = require("rk-utils");
+const remap = require('./utils/remap');
 
 exports.toBoolean = (value) => (typeof value === "boolean" ? value : validator.toBoolean(value.toString(), true));
 
@@ -42,36 +43,8 @@ exports.toSet = (arrayOfObjects, property) => {
     return result;
 }
 
-/**
- * Remap the keys of object elements in an array, like projection.
- * @param {*} object
- * @param {*} mapping - key to newKey or key to array[ newKey, valueMap ] for next level mapping
- * @param {boolean} keepUnmapped - If true, will keep those not in mapping as its original key, otherwise filter out
- */
-const mapKeysDeep = (object, mapping, keepUnmapped) => {
-    if (typeof mapping === "string") return { [mapping]: object };
-
-    let newObj = {};
-    _.forOwn(object, (v, k) => {
-        if (k in mapping) {
-            let nk = mapping[k];
-            if (Array.isArray(nk)) {
-                newObj[nk[0]] = { ...newObj[nk[0]], ...mapKeysDeep(v, nk[1], keepUnmapped) };
-            } else {
-                newObj[nk] = v;
-            }
-        } else {
-            if (keepUnmapped) {
-                newObj[k] = v;
-            }
-        }
-    });
-
-    return newObj;
-};
-
 const mapArraysDeep = (arrayOfObjects, mapping, keepUnmapped) =>
-    _.map(arrayOfObjects, (obj) => mapKeysDeep(obj, mapping, keepUnmapped));
+    _.map(arrayOfObjects, (obj) => remap(obj, mapping, keepUnmapped));
 
-exports.mapKeysDeep = mapKeysDeep;
+exports.mapKeysDeep = remap;
 exports.mapArraysDeep = mapArraysDeep;
---
+++ src/utils/remap.js
@@ -0,0 +1,31 @@
+const { InvalidArgument } = require("@genx/error");
+
+/**
+ * Remap the keys of object elements in an array, like projection.
+ * @param {*} object
+ * @param {*} mapping - key to newKey or key to array[ newKey, valueMap ] for next level mapping
+ * @param {boolean} keepUnmapped - If true, will keep those not in mapping as its original key, otherwise filter out
+ */
+function remap(object, mapping, keepUnmapped) {
+    if (typeof mapping !== "object") throw new InvalidArgument('"mapping" should be a key-value object.');
+
+    let newObj = {};
+    Object.entries(object).map(([k, v]) => {
+        if (k in mapping) {
+            let nk = mapping[k];
+            if (Array.isArray(nk)) {
+                newObj[nk[0]] = { ...newObj[nk[0]], ...remap(v, nk[1], keepUnmapped) };
+            } else {
+                newObj[nk] = v;
+            }
+        } else {
+            if (keepUnmapped) {
+                newObj[k] = v;
+            }
+        }
+    });
+
+    return newObj;
+}
+
+module.exports = remap;
