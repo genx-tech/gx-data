e04bdc68fb39913ed0c6858d741140fcc3d490cf
Rockie Guo <rockie@kitmi.com.au>
2022-04-21 17:33:54 +1000

    fix: auto timestamp with state tracking

--- src/entityFeatures/stateTracking.js
+++ src/entityFeatures/stateTracking.js
@@ -17,10 +17,13 @@ module.exports = {
                         `State "${targetState}" is not one of the pre-defined states of field "${featureItem.field}" of entity "${entityModel.meta.name}".`
                     );
                 }
-                context.latest[timestampFieldName] = Generators.default(
-                    entityModel.meta.fields[timestampFieldName],
-                    context.i18n
-                );
+
+                if (context.latest[timestampFieldName] == null) {
+                    context.latest[timestampFieldName] = Generators.default(
+                        entityModel.meta.fields[timestampFieldName],
+                        context.i18n
+                    );
+                }
             }
         });
 
